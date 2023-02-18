d1dd6663e6976d05840348622f31f1269260c0ef
ziifancy <zoeerute@gmail.com>
2023-02-17 20:43:36 -0800

    0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-[5:14 am, 18/02/2023] PRESIDENTIAL DRYCLEANERS & SHOENAMITEMAN: #include <stdio.h>
+[5:14 am, 18/02/2023] PRESIDENTIAL DRYCLEANERS & SHOENAMITEM#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -21,32 +21,4 @@ int main(void)
 		printf("%d is negative\n", n);
 
 	return (0);
-}
-[5:15 am, 18/02/2023] PRESIDENTIAL DRYCLEANERS & SHOENAMITEMAN: 1
-[5:15 am, 18/02/2023] PRESIDENTIAL DRYCLEANERS & SHOENAMITEMAN: #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int n;
-	int L;
-	char str[] = "Last digit of";
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	L = n % 10;
-
-	if (L > 5)
-		printf("%s %d is %d and is greater than 5\n", str, n, L);
-	else if (L == 0)
-		printf("%s %d is %d and is 0\n", str, n, L);
-	else if (L < 6)
-		printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
-
-	return (0);
-}
+}}
