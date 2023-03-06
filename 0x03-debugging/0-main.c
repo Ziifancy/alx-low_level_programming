9fece535671b5592c9be2809be1fa7e1483b2200
ziifancy <zoeerute@gmail.com>
2023-03-06 10:07:08 -0800

    0. memset

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -7,10 +6,12 @@
 
 int main(void)
 {
-        int i;
+ int i;
 
-        i = 98;
-        positive_or_negative(i);
+ i = 98;
+ positive_or_negative(i);
 
-        return (0);
+ return (0);
 }
+
+
