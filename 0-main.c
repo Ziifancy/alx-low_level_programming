e86919197b7095aa4b27b80f5a37cfc78d0e0481
ziifancy <zoeerute@gmail.com>
2023-03-08 12:46:10 -0800

    0-puts_recursion.c

--- 0-main.c
+++ 0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
@@ -8,15 +7,6 @@
  */
 int main(void)
 {
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
-
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strcat(s1, s2);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
+    _puts_recursion("Puts with recursion");
     return (0);
 }
