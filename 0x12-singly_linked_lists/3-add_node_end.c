6a6fb1b365936ed27e3f509e685ec971417d7096
ziifancy <zoeerute@gmail.com>
2023-03-31 16:12:42 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,50 +1,99 @@
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 #include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+
 
 /**
- * add_node_end - adds a node to the end of a linked list
- * @head: pointer to the head of the list
- * @str: string to be used as content of the newly added node
+ * _strlen - function to get length of a string
+ * @s: string
  *
- * Return: address of the newly added node
+ * Return: Length of a array of characters
  */
-list_t *add_node_end(list_t **head, const char *str)
+int _strlen(char *s)
 {
-	list_t *new_node;
-	list_t *cursor = *head;
+	int i;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node != NULL)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		new_node->str = strdup(str);
-		new_node->len = _strlen(str);
-		new_node->next = NULL;
+		i++;
 	}
-	else
-		return (NULL);
-	if (cursor != NULL)
-	{
-		while (cursor->next != NULL)
-			cursor = cursor->next;
-		cursor->next = new_node;
-	}
-	else
-		*head = new_node;
-	return (new_node);
+	return (i);
+}
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory, which
+ * contains a copy of the string given as a parameter.
+ * @str: string to copy
+ *
+ * Return: Pointer
+ */
+char *_strdup(const char *str)
+{
+	int l = 0, i;
+	char *s;
+
+	if (str == NULL)
+		return (0);
+
+	while (*(str + l))
+		l++;
+
+	s = malloc(sizeof(char) * l + 1);
+
+	if (s == 0)
+		return (0);
+
+	for (i = 0; i <= l; i++)
+		*(s + i) = *(str + i);
+	return (s);
 }
 
 /**
- * _strlen - counts the length of a string
- * @str: string to be counted
+ * add_node_end - adds a new node to a singly linked list at the end of it
+ * @head: pointer to head of the singly linked list_t
+ * @str: string to add to the new node
  *
- * Return: the length of the string
+ * Return: the address of the new element, or NULL if it failed
  */
-int _strlen(const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	int i = 0;
+	char *tmp;
+	list_t *tmph;
+	list_t *new_node = malloc(sizeof(list_t));
 
-	while (str[i] != '\0')
-		i++;
-	return (i);
+	if (new_node == 0)
+		return (0);
+
+	tmph = *head;
+	if (str == 0)
+	{
+		new_node->str = 0;
+		new_node->len = 0;
+	}
+	else
+	{
+		tmp = _strdup(str);
+		if (tmp == 0)
+		{
+			free(new_node);
+			return (0);
+		}
+		new_node->str = tmp;
+		new_node->len = _strlen(tmp);
+	}
+	new_node->next = 0;
+
+	if (tmph == 0)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		while (tmph->next != 0)
+			tmph = tmph->next;
+		tmph->next = new_node;
+	}
+	return (new_node);
 }
